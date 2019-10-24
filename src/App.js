15e3da267445bd634617ec7215e7039344db0322
Henry <hchen651@gmail.com>
2019-10-23 20:38:51 -0400

    updated navabar and index

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import Navbar from "./components/Navbar";
 import MainContainer from "./components/MainContainer";
 import './App.css';
 
 function App() {
   return (
-    function App() {
-      return (
-        <div>
-          <Navbar />
-          <MainContainer />
-        </div>
-      );
-    }
+    <div>
+      <Navbar />
+      <MainContainer />
+    </div>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <h2>Navbar Stuff here</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
