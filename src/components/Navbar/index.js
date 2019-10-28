204bd4fa9dde7dfad69b67c824b01a916b9567c1
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2019-10-28 16:45:20 -0400

    bootstrap navbar update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import './style.css';
 
 class NavBar extends React.Component {
@@ -7,7 +9,23 @@ class NavBar extends React.Component {
         return (
             <div>
                 <Navbar bg="light">
-                    <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+                    <Navbar.Brand href="#home">Game Spot</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                        Signed in as: <a href="#login">PotatoGamer001</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                        <Nav className="mr-auto">
+
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                        </Nav>
                 </Navbar>
             </div>
         );
