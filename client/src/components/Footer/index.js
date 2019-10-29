cdd1439210a80c2ad0359f39709db6e78133529f
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2019-10-28 20:36:03 -0400

    add fontawesome

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,11 +4,18 @@ import React from 'react';
 //Stylesheet
 import './style.css';
 
+//FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
+
 class Footer extends React.Component {
     render () {
         return (
             <footer>
-                <span>some footer text</span>
+                <span>Rearcade © 2019. All Rights Reserved </span>
+                <a target="_blank" href="https://github.com/hchen651/react-arcade-project">
+                    <FontAwesomeIcon icon={faGithubAlt} />
+                </a>
             </footer>
         );
     }
